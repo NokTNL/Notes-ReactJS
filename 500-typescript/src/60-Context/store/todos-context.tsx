32e0c6a6c3eec792c772d62b910e947409e60e1a
NokTNL <noklamtsz@gmail.com>
2022-08-29 09:08:10 +0100

    Improve TS Context notes

--- 500-typescript/src/60-Context/store/todos-context.tsx
+++ 500-typescript/src/60-Context/store/todos-context.tsx
@@ -1,18 +1,19 @@
 import { createContext, PropsWithChildren, useState } from "react";
-import Todo from '../models/todo';
+import Todo from "../models/todo";
 
 // Typically you can create a context like just with null value:
-const NullContext = createContext(null)
+const NullContext = createContext(null);
 
 // Or initialise with dummy inital values and hook real objects to it later on, which has the advantage of telling the shape of the context object in advance (e.g. for IDE auto-completion):
 const InitialContext = createContext({
   items: [], // <-- TS cannot infer the type here, because you can't decalre an empty Todo[] array here
   addTodo: (todoText: string) => {}, // TS cannot infer what type will be returned
-})
+});
 
 // If you do not provide concrete initial values, TS cannot infer all the required type
-// Therefore the simplest way of initialising is to tell the type in advance (that also accepts null value), and then initialise with an empty object with that type using `as`
-// This is called TYPE ASSERTION, which tells TS you are sure that it WILL have that type later on (we are sure about that as we always initialise the context value in the provider with that type)
+// Therefore the simplest way of initialising is to tell the type in advance, and then initialise with an default empty value
+// - You can use `as`, called TYPE ASSERTION, which tells TS you are sure that it WILL have that type later on (we are sure about that as we always initialise the context value in the provider with that type)
+// !!! make sure you DID INITIALISE the context's provider properly, otherwise will throw RUNTIME error when using it
 
 type TodosContextType = {
   items: Todo[];
@@ -22,6 +23,10 @@ type TodosContextType = {
 
 export const TodosContext = createContext({} as TodosContextType);
 
+// - Alternatively, tell TS that the context value can be of null type as well, using UNION type
+// Doing this and TS will warn you that the context could be null
+const TodosContext2 = createContext<TodosContextType | null>(null);
+
 // Most of the time you will make your own provider wrapper component that renders `children`. You must tell TS that children is a prop!
 // React has a handy type for using children in props, `PropsWithChildren`, that has this definition:
 /* 
@@ -30,7 +35,9 @@ export const TodosContext = createContext({} as TodosContextType);
 // Therefore, you can extend this type with:
 //  - providing the GENERIC <P>, e.g. (props: PropsWithChildren<{extraProp: string}>)
 //  - TYPE INTERSECTION, e.g. PropsWithChildren & {extraProp: string}
-const TodosContextProvider = (props: PropsWithChildren<{extraProp?: string}>) => {
+const TodosContextProvider = (
+  props: PropsWithChildren<{ extraProp?: string }>
+) => {
   //                          ^ Alternative: ({children, ...props}: PropsWithChildren<{extraProp?: string}>)
   //                          `props` will now receive the REMAINING type {extraProp?: string}
   const [todos, setTodos] = useState<Todo[]>([]);
@@ -57,7 +64,7 @@ const TodosContextProvider = (props: PropsWithChildren<{extraProp?: string}>) =>
 
   // Context.Provider has a prop called `children` as well
   // since `props` has the strucutre {children: ReactNode}, `...props` will pass the children for you like magic!
-  return <TodosContext.Provider value={contextValue} {...props} />
-}
+  return <TodosContext.Provider value={contextValue} {...props} />;
+};
 
 export default TodosContextProvider;
